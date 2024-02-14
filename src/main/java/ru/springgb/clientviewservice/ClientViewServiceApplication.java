3436d57d043ee8f675fe9858874d274030a58ae6
danil <danila_monster@mail.ru>
2024-02-14 13:45:50 +0300

    commitInit

---
+++ src/main/java/ru/springgb/clientviewservice/ClientViewServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.springgb.clientviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientViewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientViewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/springgb/clientviewservice/ClientViewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.springgb.clientviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientViewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
